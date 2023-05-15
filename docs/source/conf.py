5d7f84fb0a2de4554ba8483abc65e92600ebd9b0
Azybear1 <133623629+Azybear1@users.noreply.github.com>
2023-05-15 10:07:05 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Luan Lab'
+copyright = '2023, Rice University'
+author = 'Yingchu'
 
 release = '0.1'
 version = '0.1.0'
